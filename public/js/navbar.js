81332c38fcf3ccc583e16ad75009c5bd8620647a
Abhinav <abhinavkumar1136@outlook.com>
2021-11-30 23:37:33 +0530

    ui changes

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,14 @@
 
 const mobileScreen = window.matchMedia("(max-width: 990px )");
+console.log(mobileScreen)
 $(document).ready(function () {
+    if(mobileScreen.matches){
+        $(".menu-toggle i").show();
+        $(".close").hide();
+    }
     $(".dashboard-nav-dropdown-toggle").click(function () {
+        $(".dashboard-nav-dropdown-toggle").hide();
+        $(".close").show();
         $(this).closest(".dashboard-nav-dropdown")
             .toggleClass("show")
             .find(".dashboard-nav-dropdown")
@@ -10,7 +17,22 @@ $(document).ready(function () {
             .siblings()
             .removeClass("show");
     });
+    $(".close").click(function(){
+        $(".dashboard-nav-dropdown-toggle").show();
+        $(".menu-toggle i").show();
+        $(".close").hide();
+        if (mobileScreen.matches) {
+            $(".dashboard-nav").toggleClass("mobile-show");
+        } else {
+            $(".dashboard").toggleClass("dashboard-compact");
+        }
+            
+    });
+
+
     $(".menu-toggle").click(function () {
+        $(".menu-toggle i").hide();
+        $(".close").show();
         if (mobileScreen.matches) {
             $(".dashboard-nav").toggleClass("mobile-show");
         } else {
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -12,7 +12,7 @@
         }
         .bg-darkblue{
             /* background-color: #00043c !important; */
-            background-color: #00043c !important;
+            background-color: #fff !important;
         }
     </style>
 
@@ -22,45 +22,28 @@
         <header>
             <a href="#!" class="menu-toggle">
                 <i class="fas fa-bars"></i>
-            </a>
-            <a href="#"class="brand-logo"><i
-                class="fas fa-anchor"></i> <span>BRAND</span></a></header>
+            </a> 
+            <div href="#!" class="close" style="
+                    display: inline;
+                    position: absolute;
+                    left: 30px;
+                    font-size: 30px;
+                ">
+                <i class="fas fa-times"></i>
+            </div>
+           </header>
         <nav class="dashboard-nav-list"><a href="#" class="dashboard-nav-item"><i class="fas fa-home"></i>
-            Home </a><a
-                href="#" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i> dashboard
-        </a><a
-                href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Upload </a>
-            <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
-                    class="fas fa-photo-video"></i> Media </a>
-                <div class='dashboard-nav-dropdown-menu'><a href="#"
-                                                            class="dashboard-nav-dropdown-item">All</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Recent</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Images</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Video</a></div>
-            </div>
-            <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
-                    class="fas fa-users"></i> Users </a>
-                <div class='dashboard-nav-dropdown-menu'><a href="#"
-                                                            class="dashboard-nav-dropdown-item">All</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Subscribed</a><a
-                        href="#"
-                        class="dashboard-nav-dropdown-item">Non-subscribed</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Banned</a><a
-                        href="#" class="dashboard-nav-dropdown-item">New</a></div>
-            </div>
-            <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i
-                    class="fas fa-money-check-alt"></i> Payments </a>
-                <div class='dashboard-nav-dropdown-menu'><a href="#"
-                                                            class="dashboard-nav-dropdown-item">All</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Recent</a><a
-                        href="#" class="dashboard-nav-dropdown-item"> Projections</a>
-                </div>
-            </div>
-            <a href="#" class="dashboard-nav-item"><i class="fas fa-cogs"></i> Settings </a><a
-                    href="#" class="dashboard-nav-item"><i class="fas fa-user"></i> Profile </a>
-          <div class="nav-item-divider"></div>
-          <a
-                    href="#" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i> Logout </a>
+            Home </a>
+                 <a href="#" class="dashboard-nav-item active"><i class="fa fa-user"></i> Join as Rookie
+                 </a>
+                 <a href="#" class="dashboard-nav-item"><i class="fa fa-user"></i>Login</a>
+                 <a href="#" class="dashboard-nav-item">FAQ</a>
+                 <a href="#" class="dashboard-nav-item">What is Morgi</a>
+                 <a href="#" class="dashboard-nav-item">A word from our president</a>
+                 <a href="#" class="dashboard-nav-item">Our mission</a>
+                 <a href="#" class="dashboard-nav-item">Who we are</a>
+                 <a href="#" class="dashboard-nav-item"><i class="fa fa-file"></i>Legal and Policies</a>
+          
         </nav>
     </div>
     <div class='dashboard-app mt-0'>
@@ -83,7 +66,7 @@
         <div class="row">
         <div class="col-md-12  px-0 bg-darkblue" >
             <div class="d-flex justify-content-between my-3 px-4">
-                <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
+            <a href="#!" class="menu-toggle" ><i class="fas fa-bars" style="display:none;"></i></a>
                 <img src="{{ asset('images/logo.png') }}" />
                 <a href="#" class="btn btn-warning rounded font-weight-bolder btn-sm b-radius-17 align-self-center">Join Morgi</a>
             </div>
